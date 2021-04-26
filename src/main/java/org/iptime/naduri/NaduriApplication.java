8128fc79ed2e4c4fea181b8ae77e1f183cb045e5
2goni <gh2684@naver.com>
2021-04-26 16:19:16 +0900

    프로젝트 기본설정

---
+++ src/main/java/org/iptime/naduri/NaduriApplication.java
@@ -0,0 +1,13 @@
+package org.iptime.naduri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaduriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaduriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iptime/naduri/NaduriApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iptime.naduri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaduriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

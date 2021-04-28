b4f59ce04d53189fb73c3695de57fcc03eb7fec3
origin <chldlsrb1000@naver.com>
2021-04-28 09:12:06 +0900

    ModuleManagement 생성

--- src/main/java/org/iptime/naduri/domain/module_management/ModuleManagement.java
+++ src/main/java/org/iptime/naduri/domain/module_management/ModuleManagement.java
@@ -1,4 +1,32 @@
 package org.iptime.naduri.domain.module_management;
 
-public class ModuleManagement {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.iptime.naduri.domain.BaseTimeEntity;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class ModuleManagement extends BaseTimeEntity {
+
+    //회원정보의 모듈 정보, 복합키(회원정보, 모듈정보)
+    @EmbeddedId
+    private ModuleManagementId moduleManagementId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Progress progress;
+
+    @Builder
+    public ModuleManagement(ModuleManagementId moduleManagementId, Progress progress){
+        this.moduleManagementId = moduleManagementId;
+        this.progress = progress;
+    }
+
+    public void update(Progress progress){
+        this.progress = progress;
+    }
 }
---
+++ src/main/java/org/iptime/naduri/domain/module_management/ModuleManagementRepository.java
@@ -0,0 +1,6 @@
+package org.iptime.naduri.domain.module_management;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModuleManagementRepository extends JpaRepository<ModuleManagement, ModuleManagementId> {
+}

96efcf9ed38a230668316c7c1636bab6444c18d7
origin <chldlsrb1000@naver.com>
2021-04-28 09:22:46 +0900

    CourseInModule 생성

--- src/main/java/org/iptime/naduri/domain/course_in_module/CourseInModule.java
+++ src/main/java/org/iptime/naduri/domain/course_in_module/CourseInModule.java
@@ -1,4 +1,37 @@
 package org.iptime.naduri.domain.course_in_module;
 
-public class CourseInModule {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.iptime.naduri.domain.BaseTimeEntity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+//코스의 모듈 정보
+@Getter
+@NoArgsConstructor
+@Entity
+public class CourseInModule extends BaseTimeEntity {
+    //코스의 모듈, 복합키(코스정보, 모듈정보)
+    @EmbeddedId
+    private CourseInModuleId courseInModuleId;
+
+    //코스의 모듈의 태그
+    private String courseInModuleTag;
+
+    //코스의 모듈의 내용
+    private String courseInModuleContents;
+
+    @Builder
+    public void setCourseInModule(CourseInModuleId courseInModuleId, String courseInModuleTag, String courseInModuleContents){
+        this.courseInModuleId = courseInModuleId;
+        this.courseInModuleTag = courseInModuleTag;
+        this.courseInModuleContents = courseInModuleContents;
+    }
+
+    public void update(String courseInModuleTag, String courseInModuleContents){
+        this.courseInModuleTag = courseInModuleTag;
+        this.courseInModuleContents = courseInModuleContents;
+    }
 }
---
+++ src/main/java/org/iptime/naduri/domain/course_in_module/CourseInModuleId.java
@@ -0,0 +1,37 @@
+package org.iptime.naduri.domain.course_in_module;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.iptime.naduri.domain.course.Course;
+import org.iptime.naduri.domain.module.Module;
+import org.iptime.naduri.domain.user_info.UserInfo;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+//코스의 모듈정보 복합키
+@Getter
+@NoArgsConstructor
+@Embeddable
+public class CourseInModuleId implements Serializable {
+    //코스정보, 외래키 설정
+    @ManyToOne()
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "courseId")
+    private Course course;
+
+    //모듈정보, 외래키 설정
+    @ManyToOne()
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "moduleId")
+    private Module module;
+
+    public void setUserFavoritesId(Course course, Module module){
+        this.course = course;
+        this.module = module;
+    }
+}
